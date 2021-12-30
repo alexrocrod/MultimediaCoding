c0070439b71e6400e2cc84b4cc3bd2ea96d7b950
Alexandre Rodrigues <alexandrerrodrigues@ua.pt>
2021-12-30 17:34:50 +0000

    added relevant code to main and deleted unnecessary scripts

--- Matlab/proj.m
+++ Matlab/proj.m
@@ -16,11 +16,40 @@ clear all
 
 %% Parameters
 
+L = 2; % 2 or 4
+
+if L ==2
+    Rs = [2,4];
+else
+    Rs = [1,2];
+end
+
+%% Load Audio
+filename = 'Audio\70stereo.wav';
+
+info = audioinfo(filename);
+[x,F] = audioread(filename,'native') ; 
+fprintf('\n'); 
+fprintf('Sampling frequency:      F = %d',F); fprintf(' [Hz] \n'); 
+fprintf('Resolution:          nbits = %d',info.BitsPerSample); fprintf(' [bit] \n');
+
+if info.BitsPerSample == 8
+    x = int16(x) - 127;
+end
+
+%% Convert to mono (needed ??)
+
+if info.NumChannels == 2
+    xmono = int16(mean(x,2));
+end
+
+%% 
 
 
 
 
-%%
+
+
 
 
 
